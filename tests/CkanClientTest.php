13f87eb453f2b098b05f9ab057b8ef81b4eb8bde
Alex Perfilov <alexandr.perfilov@reisystems.com>
2015-03-31 22:46:19 -0400

    travis, phpunit test

---
+++ tests/CkanClientTest.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: alexandr.perfilov
+ * Date: 3/31/15
+ * Time: 4:38 PM
+ */
+
+namespace CKAN\Client;
+
+
+class CkanClientTest extends \BaseTestCase {
+
+    public function setUp()
+    {
+        $this->CkanClient = $this->getMockBuilder('CkanClient')
+            ->disableOriginalConstructor()
+            ->getMock();
+//        $this->testClass = 'CkanClient';
+//        parent::setup();
+    }
+
+    public function testSetHeaders()
+    {
+        $this->assertTrue(true);
+    }
+}
